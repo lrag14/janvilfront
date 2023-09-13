2bffd9ca51ee8e64504cf48dd0d2b780268274a5
lrag14 <laurentrober@gmail.com>
2023-09-13 20:57:48 +0200

    sylvie +json

--- src/content/js/app.js
+++ src/content/js/app.js
@@ -1,3 +1,16 @@
 document.getElementById("inscription").addEventListener("submit", function () {
   alert("Formulaire envoyé !");
 });
+
+function contacts_new() {
+  const c = new Contact(
+    null,
+    document.querySelector("#name").value,
+    document.querySelector("#number").value,
+    document.querySelector("#message").value,
+  );
+
+  if (c.save()) {
+    alert("Created New Contact!");
+  }
+}
